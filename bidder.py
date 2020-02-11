a1d43eec30eb6c97336ac5c5330c87023ee8f937
Ayrton Trujillo <ayrtonx123@gmail.com>
2020-02-10 20:40:06 -0500

    keep track of registered accounts

--- bidder.py
+++ bidder.py
@@ -6,35 +6,107 @@ import os
 import requests
 import csv
 import json
+import pandas as pd
+import numpy as np
 
+COLUMNS = ['Email','Card Type','Card Number','Card Expiry','CVV','Country','Phone Number','First Name','Last Name','Address 1','Address 2','Zip Code','City','State']
 # Gather our code in a main() function
 def main():
   proxiesFile = 'proxies.txt'
   accountsFile = 'profiles.csv'
 
-  proxies = readProxies(proxiesFile)
-  accounts = readAccounts(accountsFile)
+  # #proxies = readProxies(proxiesFile)
+  accounts = registerAccounts(accountsFile)
 
-  print(json.dumps(accounts, indent=4))
+  # print(json.dumps(accounts, indent=4))
 
   # registered = registerAccounts(accounts)
 
+  #login
+  # login()
 
-
-def readAccounts(accountsFile):
-  if not os.path.isfile(accountsFile):
-    print(f"File does not exist. {accountsFile} Exiting.")
+def registerAccounts(profilesFile):
+  if not os.path.isfile(profilesFile):
+    print(f"File does not exist. {profilesFile} Exiting.")
     sys.exit()
 
-  accounts = []
+  # profiles = []
 
-  with open(accountsFile) as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-      accounts.append(row)
+  profiles = pd.read_csv(profilesFile)
 
-  return accounts
+  for index, profile in profiles.iterrows():
+    email = profile['Email']
 
+    if isRegistered(profile):
+      print(f'{email} is already registered')
+      continue
+
+    else:
+      print(f'registering {email}')
+      first = profile['First Name']
+      last = profile['Last Name']
+      if(signup(email, first, last)):
+        markRegistered(profile) 
+  return profiles
+
+def isRegistered(profile):
+  registered = pd.read_csv('registered.csv')
+
+  if registered['Email'].str.contains(profile[0], regex=False).any():
+    return True
+  else:
+    return False
+
+def signup(email, first, last):
+  headers = {'accept': '*/*', 'origin': 'https://accounts.stockx.com', 'content-type': 'application/json'}
+  success = False
+
+  body = {
+    "connection": "production",
+    "email": email,
+    "password": "cookieSnores1",
+    "user_metadata": {
+        "first_name": first,
+        "last_name": last,
+        "language": "en-us",
+        "gdpr": ""
+    }
+  }
+  print(json.dumps(headers, indent=4))
+  print(json.dumps(body,indent=4))
+    
+  r = requests.post('https://accounts.stockx.com/dbconnections/signup', headers=headers, json=body)
+  print(r.text)
+  if(r.status_code == 200):
+    print(f'Account {email} was successfully registered.')
+    success = True
+    
+  print(f'headers: {r.headers}')
+  print(f'cookies: {r.cookies}')
+
+  return success
+
+def markRegistered(profile):
+  df = pd.DataFrame(columns=COLUMNS)
+  account = df.append(profile).set_index('Email')
+  account.to_csv('registered.csv', mode='a', header=False)
+
+# def login():
+
+  #   url = "https://gateway.stockx.com/stage/v1/login"
+
+  #   payload = {
+  #     'email': 'noctuafinancial+2@gmail.com',
+  #     'password': 'snoringCooki1'
+  #   }"{\n\t\"email\": \"{{email}}\",\n\t\"password\": \"{{password}}\"\n}"
+  #   headers = {
+  #   'Accept': 'application/json',
+  #   'Content-Type': 'application/json',
+  #   }
+
+  #   response = requests.post(url, headers=headers, json = payload)
+
+  #   print(response.text.encode('utf8'))
 
 
 def readProxies(proxiesFile):
